027ca80567666e75193759dc934e9733c674845a
GusBordoni <107891199+GusBordoni@users.noreply.github.com>
2023-07-12 17:04:39 -0300

    adding and removing pixel divs

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
-const gridContainer = document.querySelector('.grid-container');
+const gridContainer = document.querySelector('#grid-container');
+const gridSlider = document.querySelector('#grid-slider');
+const gridValue = document.querySelector('#grid-value');
+const slider = document.querySelector('#slider');
 let pixelCount = 256;
+let mouseDown = false;
 
-for (let i = 0; i < pixelCount; i++) {
-    const pixel = document.createElement('div');
-    pixel.classList.add('pixel');
-    gridContainer.appendChild('pixel');
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+slider.addEventListener('change', function(){
+    setTimeout(() => {
+        pixels.forEach(pixel => {
+            pixel.remove();
+        })
+        console.log('all removed');
+        /* console.log('renova grid ' + Math.ceil((slider.value/2)**2));
+        pixelCount = Math.ceil((slider.value/2)**2);
+        setGrid(pixelCount); */
+        console.log('atempting to add '+ pixelCount + ' pixels now');
+        setGrid(32);
+    }, 2000);
+})
+
+slider.addEventListener('input', function(){
+    gridValue.textContent = slider.value;
+})
+
+
+
+function setGrid(n){
+    for (let i = 0; i < n; i++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        gridContainer.appendChild(pixel);
+        pixel.addEventListener('mouseover', changeColor);
+        pixel.addEventListener('mousedown', changeColor);
+    }
 }
+setGrid(16);
 
+let pixels = document.querySelectorAll('.pixel');
 
-
-/* pixel.forEach(pixel => gridPixel.addEventListener('click', console.log('ok'))); */
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return;
+    if (e.type === 'mousedown' || mouseDown) {
+        e.target.style.backgroundColor = 'black';
+    }
+}
 
